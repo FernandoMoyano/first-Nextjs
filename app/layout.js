6090c9a493a0db99f11c454a4a6e982ddbe3f190
FernandoMoyano <fernandomoyano21@gmail.com>
2023-10-03 21:03:01 -0300

    Fonts

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
 import { Navbar } from "../components/Navbar";
+import { Poppins, Roboto } from "next/font/google";
 
 export const metadata = {
   title: "Next.js",
   description: "Comercio",
   keywords: "e-commerce, online ",
 };
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["italic", "normal"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
